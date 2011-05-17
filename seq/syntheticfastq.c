8bcc22b4bba51ea6d89120dcd4bb5d854628d584
Andre Masella <andre@masella.no-ip.org>
2011-05-17 15:07:36 -0400

    Produce synthetic data for PANDA testing

---
+++ seq/syntheticfastq.c
@@ -0,0 +1,116 @@
+/* Apply quality masks from real data onto synthetic data. */
+#include<bzlib.h>
+#include<ctype.h>
+#include<errno.h>
+#if !defined(__APPLE__)
+#include<error.h>
+#endif
+#include<fcntl.h>
+#include<math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<time.h>
+#include<unistd.h>
+#include<zlib.h>
+#include "kseq.h"
+
+/* Function pointers for file I/O such that we can deal with compressed files. */
+void *(*fileopen) (char *, char *) = (void *(*)(char *, char *))gzopen;
+int (*fileread) (void *, void *, int) = (int (*)(void *, void *, int))gzread;
+int (*fileclose) (void *) = (int (*)(void *))gzclose;
+
+/* Compatibility function to make BZ2_bzRead look like gzread. */
+int bzread(BZFILE * file, void *buf, int len)
+{
+	int bzerror = BZ_OK;
+	int retval = BZ2_bzRead(&bzerror, file, buf, len);
+	if (bzerror == BZ_OK || bzerror == BZ_STREAM_END) {
+		return retval;
+	} else {
+		fprintf(stderr, "bzip error %d\n", bzerror);
+		return -1;
+	}
+}
+
+KSEQ_INIT(void *, fileread)
+
+int main(int argc, char **argv)
+{
+	int c;
+	int bzip = 0;
+	char *filename = NULL;
+	void *file;
+	kseq_t *seq;
+	int len;
+	char *syn;
+	int synlen;
+
+	/* Process command line arguments. */
+	while ((c = getopt(argc, argv, "jf:")) != -1) {
+		switch (c) {
+		case 'j':
+			fileopen = (void *(*)(char *, char *))BZ2_bzopen;
+			fileread = (int (*)(void *, void *, int))bzread;
+			fileclose = (int (*)(void *))BZ2_bzclose;
+			bzip = 1;
+			break;
+		case 'f':
+			filename = optarg;
+			break;
+		case '?':
+			if (optopt == (int)'f') {
+				fprintf(stderr,
+					"Option -%c requires an argument.\n",
+					optopt);
+			} else if (isprint(optopt)) {
+				fprintf(stderr,
+					"Unknown option `-%c'.\n", optopt);
+			} else {
+				fprintf(stderr,
+					"Unknown option character `\\x%x'.\n",
+					(unsigned int)optopt);
+			}
+			return 1;
+		default:
+			abort();
+		}
+	}
+
+	if (filename == NULL || optind != argc - 1) {
+		fprintf(stderr,
+			"Usage: %s [-j] -f file.fastq sequence\n\t-j\tInput files are bzipped.\n",
+			argv[0]);
+		return 1;
+	}
+
+	syn = argv[optind];
+	synlen = strlen(syn);
+
+	/* Open files and initialise FASTQ reader. */
+	file = fileopen(filename, "r");
+	if (file == NULL) {
+		perror(filename);
+		return 1;
+	}
+	seq = kseq_init(file);
+	while ((len = kseq_read(seq)) >= 0) {
+		int max = seq->qual.l < synlen ? seq->qual.l : synlen;
+		int i;
+
+		printf("@%s\n", seq->name.s);
+		for (i = 0; i < max; i++) {
+			fputc((int) syn[i], stdout);
+		}
+		printf("\n+%s\n", seq->name.s);
+		for (i = 0; i < max; i++) {
+			fputc((int) seq->qual.s[i], stdout);
+		}
+		fputc((int)'\n', stdout);
+	}
+	kseq_destroy(seq);
+	if (fileclose(file) != Z_OK && bzip == 0) {
+		perror(filename);
+	}
+	return 0;
+}
